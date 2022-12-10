6dbf600dec7b4012a6a64cbdcdbaa318683b7b68
Peter van der Heijden <enzopeter02@gmail.com>
2022-12-10 21:09:28 +0100

    changed code to typescript

--- index.js
+++
@@ -1,8 +0,0 @@
-const { resolve } = require('path');
-
-/**
- * Create an absolute path to the root directory.
- * @param {string} path - a relative path
- * @returns {string} - absolute path
- */
-module.exports = (path) => resolve(path || '')
